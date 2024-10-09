3156f5713a224da91877a3d37485c9e2a09478af
aditya <adity119@gmail.com>
2024-10-09 12:39:39 +0530

    create model and serializer for inventory

--- inventory_management_system/inventory/models.py
+++ inventory_management_system/inventory/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class InventoryItem(models.Model):
+    name = models.Charfield(max_length=200, unique=True, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    def __str__ (self):
+        return self.name
